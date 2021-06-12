87e9058fc394fb526e04d3f0c1ad09a9a4dd1c0d
Ciro S. Costa <utxobr@protonmail.com>
2021-06-12 09:12:55 -0400

    pkg/rpc: document dump transport
    Signed-off-by: Ciro S. Costa <utxobr@protonmail.com>

--- pkg/rpc/dump_transport.go
+++ pkg/rpc/dump_transport.go
@@ -6,16 +6,28 @@ import (
 	"net/http/httputil"
 )
 
+// DumpTransport implements the `net/http.RoundTripper` interface wrapping
+// another Roundtripper dumping to stdout both the requests and the responses
+// that it sees passing through.
+//
 type DumpTransport struct {
 	R http.RoundTripper
 }
 
+// NewDumpTransport instantiates a new DumpTransport.
+//
 func NewDumpTransport(rt http.RoundTripper) *DumpTransport {
 	return &DumpTransport{
 		R: rt,
 	}
 }
 
+// RoundTrip implements the functionality of dumping http requests and
+// responses to `stdout` for each HTTP transaction that passes through it.
+//
+// It does so by first dumping the request, then passing that down to the
+// wrapped roundtripper, and then from the response it sees, dumping it too.
+//
 func (d *DumpTransport) RoundTrip(h *http.Request) (*http.Response, error) {
 	dump, _ := httputil.DumpRequestOut(h, true)
 	fmt.Println(string(dump))
