98e24e202de1f145409563cf15398f13ece9d77b
Ciro S. Costa <utxobr@protonmail.com>
2021-06-12 11:59:07 -0400

    pkg/http: document client constructor
    Signed-off-by: Ciro S. Costa <utxobr@protonmail.com>

--- pkg/http/client.go
+++ pkg/http/client.go
@@ -5,6 +5,11 @@ import (
 	"time"
 )
 
+// NewHTTPClient instantiates a new `http.Client` with a few defaults.
+//
+// `verbose`: if set, adds a transport that dumps all requests and responses to
+// stdout.
+//
 func NewHTTPClient(verbose bool) *http.Client {
 	client := &http.Client{
 		Timeout: 15 * time.Second,
