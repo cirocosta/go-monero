2688c5492902ddc0694db5761753e98f72067181
Ciro S. Costa <utxobr@protonmail.com>
2021-07-10 22:34:32 -0400

    prepare for release
    - making use of `goreleaser`, we can make building for all platforms
    easy peasy, leveraging go's ability to create reproducible builds given
    a set of flags that we can have under in a declarative fashion under
    `goreleaser.yaml`
    
    Signed-off-by: Ciro S. Costa <utxobr@protonmail.com>

--- cmd/monero/main.go
+++ cmd/monero/main.go
@@ -11,15 +11,29 @@ import (
 	"github.com/cirocosta/go-monero/cmd/monero/commands/wallet"
 )
 
+var (
+	version = "dev"
+	commit  = "dev"
+)
+
 var rootCmd = &cobra.Command{
 	Use:   "monero",
 	Short: "Daemon, Wallet, and p2p command line monero CLI",
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "print the version of this cli",
+	Run: func(_ *cobra.Command, _ []string) {
+		fmt.Println(version, commit)
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(daemon.RootCommand)
 	rootCmd.AddCommand(wallet.RootCommand)
 	rootCmd.AddCommand(p2p.RootCommand)
+	rootCmd.AddCommand(versionCmd)
 }
 
 func main() {
